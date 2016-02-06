1fa23032f597b76f96ba227c2b4de5e03888f904
yrx0619 <yrx0619@126.com>
2016-02-06 17:24:54 +0800

    <feat>(view-xml):add two buttons , one is clear button, other is delete button.add a function clear number and delete one digital. fix a function that input a number is ahead with 0 what can delete zero at first of all digitals.

--- app/src/main/java/activitytest/yerx/com/caculatortest/MainActivity.java
+++ app/src/main/java/activitytest/yerx/com/caculatortest/MainActivity.java
@@ -11,11 +11,14 @@ import android.widget.TextView;
 public class MainActivity extends Activity implements View.OnClickListener {
     Button[] m_buttonNum;
     Button[] m_buttonOp;
+    Button m_buttonClear;
+    Button m_buttonDel;
     //Button m_buttonDot;
     TextView m_resultView;
     int m_result;
     int[] m_number_queue;
     int[] m_op_queue;
+    String num;
 
     private void getBtnNumById(){
         m_buttonNum[0] = (Button)findViewById(R.id.button_num0);
@@ -33,6 +36,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         m_buttonOp[2] = (Button) findViewById(R.id.button_op_multiplicative);
         m_buttonOp[3] = (Button) findViewById(R.id.button_op_division);
         m_buttonOp[4] = (Button) findViewById(R.id.button_op_equal);
+        m_buttonClear = (Button) findViewById(R.id.button_clear);
+        m_buttonDel = (Button) findViewById(R.id.button_delete);
 
     }
 
@@ -46,6 +51,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
         int index = 0;
         for (; index < 5; index++)
             m_buttonOp[index].setOnClickListener(this);
+    }
+
+
+    private void setBtnClearClickListener() {
+        m_buttonClear.setOnClickListener(this);
+    }
+
+    private void setBtnDelClickListener() {
+        m_buttonDel.setOnClickListener(this);
     }
 
     private Boolean isNotNumberButton(int id)
@@ -65,6 +79,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private Boolean isPlusOperation(int op) {
         if (1 == op)
+            return Boolean.TRUE;
+        else
+            return Boolean.FALSE;
+    }
+
+    private Boolean isClearButton(int id) {
+        if (R.id.button_clear == id)
+            return Boolean.TRUE;
+        else
+            return Boolean.FALSE;
+    }
+
+    private Boolean isDelButton(int id) {
+        if (R.id.button_delete == id)
             return Boolean.TRUE;
         else
             return Boolean.FALSE;
@@ -114,6 +142,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    private void removedZeroInNumberHead() {
+        if (num.startsWith("0") && (num.length() > 1))
+            num = num.substring(1, num.length());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -125,41 +158,62 @@ public class MainActivity extends Activity implements View.OnClickListener {
         intialNumberQueue();
         intialOperationQueue();
         getBtnNumById();
+        num = "";
         m_resultView = (TextView) findViewById(R.id.caculator_text);
         //m_buttonNum[1].setOnClickListener(this);
-        Log.d("Create", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
+        //Log.d("Create", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
         setBtnNumClickListener();
         setBtnOpClickListener();
+        setBtnClearClickListener();
+        setBtnDelClickListener();
     }
 
     @Override
     public void onClick(View v) {
-        String num;
+
         Button tmp_btn;
         String result;
 
-        if (isNotNumberButton(v.getId()) == Boolean.FALSE) {
+        if (isClearButton(v.getId())) {
+            num = "";
+            m_result = 0;
+            m_number_queue[0] = 0;
+            m_number_queue[1] = 0;
+            m_op_queue[0] = 0;
+            m_op_queue[1] = 0;
+            m_resultView.setText("0");
+        } else if (isDelButton(v.getId())) {
+            String m_tmp;
+            m_tmp = m_resultView.getText().toString();
+            m_tmp = m_tmp.substring(0, m_tmp.length() - 1);
+            if (m_tmp.length() >= 1)
+                m_resultView.setText(m_tmp);
+            else
+                m_resultView.setText("0");
+        } else if (isNotNumberButton(v.getId()) == Boolean.FALSE) {
             tmp_btn = (Button) findViewById(v.getId());
-            num = tmp_btn.getText().toString();
-            Log.d("click Num", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
+            num = num + tmp_btn.getText().toString();
+            removedZeroInNumberHead();
+            //Log.d("click Num", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
             m_resultView.setText(num);
-        }
-        if (isNotNumberButton(v.getId()) == Boolean.TRUE) {
-            Log.d("141", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
+        } else if (isNotNumberButton(v.getId()) == Boolean.TRUE) {
+            //Log.d("141", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
             if (getCountInOperationQueue() == 0) {
                 num = m_resultView.getText().toString();
                 m_number_queue[0] = Integer.parseInt(num);
                 addInOperationQueue(v.getId(), 0);
-                Log.d("click Op", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
+                num = "";
+                //Log.d("click Op", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
 
             } else if (getCountInOperationQueue() == 1) {
                 num = m_resultView.getText().toString();
                 m_number_queue[1] = Integer.parseInt(num);
                 addInOperationQueue(v.getId(), 1);
-                Log.d("click op2", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
+                num = "";
+                //Log.d("click op2", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
             }
             if (getCountInOperationQueue() == 2) {
-                Log.d("click op3", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
+                //Log.d("click op3", m_op_queue[0] + "," + m_op_queue[1] + "," + m_number_queue[0] + "," + m_number_queue[1]);
                 if (isPlusOperation(m_op_queue[0])) {
                     m_result = m_number_queue[0] + m_number_queue[1];
 
@@ -168,6 +222,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 m_number_queue[0] = m_result;
                 m_op_queue[0] = m_op_queue[1];
                 m_op_queue[1] = 0;
+                num = "";
                 result = Integer.toString(m_result);
                 m_resultView.setText(result);
             }
